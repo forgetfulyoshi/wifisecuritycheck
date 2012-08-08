206ccdb55d3b706cd34387e1be5a28c75227a9eb
Ben Anglin <ben.anglin@gmail.com>
2012-08-08 18:58:51 -0400

    Renamed to DetailsActivity and fixed menu bug which would cause a crash in 2.x

--- src/com/routerraiders/wifisecuritycheck/DetailsActivity.java
+++ src/com/routerraiders/wifisecuritycheck/DetailsActivity.java
@@ -2,20 +2,24 @@ package com.routerraiders.wifisecuritycheck;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
-public class ExplanationActivity extends Activity {
+public class DetailsActivity extends Activity {
 
     @TargetApi(11)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_explanation);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_details);
+        
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+	    getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
         
         WebView web = (WebView)this.findViewById(R.id.explanation_webview);
         web.loadUrl("file:///android_asset/warn_open_wifi.html");
